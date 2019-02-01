06b1e236299871aebc6996fee1298c840852de96
peter heinum <peter.heinum@gmail.com>
2019-02-01 17:12:15 +0100

    added spotify auth flow and add to playlist function

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express');
+require('dotenv').config()
+const express = require('express')();
 const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
+const fetch = require('node-fetch')
+const querystring = require('querystring')
+const request = require('request')
 
-app.get('/', (req, res) => {
+express.use(bodyParser.json());
+express.use(bodyParser.urlencoded({ extended: true }));
+express.listen(8888, () => console.log('Webhook server is listening, port 8888'));
+
+const verify_token = process.env.VERIFY_TOKEN;
+
+express.get('/', (req, res) => {
     res.send("Hello darling");
 })
 
-app.get('/webhook', (req, res) => {
+express.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-   
+    let VERIFY_TOKEN = verify_token;
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
@@ -35,7 +41,7 @@ app.get('/webhook', (req, res) => {
     }
   });
 
- app.post('/webhook', (req, res) => {   
+ express.post('/webhook', (req, res) => {   
     let body = req.body;  
     // Checks this is an event from a page subscription
     if (body.object === 'page') {  
@@ -44,12 +50,90 @@ app.get('/webhook', (req, res) => {
         // Gets the message. entry.messaging is an array, but 
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
-        console.log(webhook_event.message.text);
+        let songlink = webhook_event.message.text;
+        if(songlink != undefined){
+          songlink = songlink.split('track/')[1];
+          let songUri = `spotify%3Atrack%3A${songlink.split('?')[0]}`;
+          addToSpotifyPlaylist(songUri);
+          console.log(songUri);
+        } else {
+          console.log(songlink)
+        }
+
+        
       });  
       // Returns a '200 OK' response to all requests
       res.status(200).send('EVENT_RECEIVED');
     } else {
       // Returns a '404 Not Found' if event is not from a page subscription
       res.sendStatus(404);
-    }  
+    }     
   });
+
+
+
+
+
+let token;
+let refreshToken;
+const playlist_id = '6D4wR8Gxo1vjBc8UIrr6fO';
+
+const auth = () => { }
+const addToPlaylist = () => { }
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
+const redirect_uri = "";
+
+express.get('/login', (req, res) => {
+
+  const queryParams = querystring.stringify({
+      client_id,
+      response_type: 'code',
+      scope: 'playlist-modify-public',
+      redirect_uri: 'http://localhost:8888/persistToken'
+  })
+
+  res.redirect('https://accounts.spotify.com/authorize?' + queryParams)
+})
+
+express.get('/persistToken', (req, res) => {
+  token = req.query.code;
+
+  const options = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+          'grant_type': 'authorization_code',
+          'code': token,
+          'redirect_uri': 'http://localhost:8888/persistToken'
+      },
+      headers: {
+          Authorization: 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+  }
+
+  request.post(options, (err, response, body) => {
+      token = body.access_token;
+      refreshToken = body.refresh_token;
+      res.send(token);
+  })
+})
+
+
+function addToSpotifyPlaylist(songUri){
+  fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks?uris=${songUri}`, {
+      method: "POST",
+      'Content-Type':'application/json',
+      'headers': {       
+          'Authorization': `Bearer ` + token
+      },
+  }).then((response) => {
+      console.log(response);
+      res.send(response);
+  })
+}
+
+
+
+  
